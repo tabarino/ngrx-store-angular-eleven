24b0430d18cfb616038dc8e1dab653f49e4f16a0
tabarino <tabarino@outlook.com>
2020-11-28 20:14:57 +0000

    Creating Reducer

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductsRoutingModule } from './products-routing.module';
-
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store';
 import * as fromComponents from './components';
 import * as fromContainers from './containers';
 import * as fromServices from './services';
@@ -17,7 +18,8 @@ import * as fromServices from './services';
         CommonModule,
         ReactiveFormsModule,
         HttpClientModule,
-        ProductsRoutingModule
+        ProductsRoutingModule,
+        StoreModule.forFeature('products', reducers)
     ],
     exports: [
         ...fromContainers.containers,
