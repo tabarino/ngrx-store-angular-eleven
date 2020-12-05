914b91b20fb5d35fdd276cd387aa015b24c00319
tabarino <tabarino@outlook.com>
2020-12-05 23:13:01 +0000

    Custom Serializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { environment } from '../environments/environment';
 import { metaReducers } from './loggerReducers';
-import { reducers } from './store';
+import { reducers, CustomSerializer } from './store';
 
 @NgModule({
     declarations: [
@@ -25,7 +24,9 @@ import { reducers } from './store';
         EffectsModule.forRoot([]),
         StoreRouterConnectingModule.forRoot()
     ],
-    providers: [],
+    providers: [
+        { provide: RouterStateSerializer, useClass: CustomSerializer }
+    ],
     bootstrap: [
         AppComponent
     ]
--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,4 +1,4 @@
-import { Params } from '@angular/router';
+import { ActivatedRouteSnapshot, Params, RouterStateSnapshot } from '@angular/router';
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 import * as fromRouter from '@ngrx/router-store';
 
@@ -17,3 +17,19 @@ export const reducers: ActionReducerMap<State> = {
 };
 
 export const getRouterState = createFeatureSelector<fromRouter.RouterReducerState<RouterStateUrl>>('routerReducer');
+
+export class CustomSerializer implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+    serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+        const { url } = routerState;
+        const { queryParams } = routerState.root;
+
+        let state: ActivatedRouteSnapshot = routerState.root;
+        while (state.firstChild) {
+            state = state.firstChild;
+        }
+        const { params } = state;
+
+
+        return { url, queryParams, params };
+    }
+}
